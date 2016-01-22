a4311372d9e0241efcffbcdff77848c613a5b60e
cldeluna <cldeluna@yahoo.com>
2016-01-22 07:13:09 -0800

    Added comments and user input to delete the tenant if it exists

--- createVlan2epg.py
+++ createVlan2epg.py
@@ -74,15 +74,31 @@ def print_tenant(session):
 
 
 def main():
+    """
+    This is the main function of the script. From here, other functions are called.
+    This script will take in the IP and credentials for an APIC, a new Tenant name, and a filename of existing VLANS
+    This script can be used when the initial ACI Policy will map EPGs to existing client vlans
+    This script takes in a list of existing vlans and creates the Tenant and corresponding VRF,ANP, EPG, and BO
+    based on naming best practice.
 
-    epg_anp_dict = {}
+    This script configures one ANP, mapped to one EPG and its corresponding BD for each vlan.
+    Each BD is a member of one VRF.
 
+    """
+    # Put relevant arguments passed to the script when the script was called in variables with east to understand names
     hostname, username, password, tenant_name, vlan_csv = sys.argv[1:]
+
+    # Initialize and empty dictionary to hold the ANP=>EPG key value pairs
+    epg_anp_dict = {}
 
     print "\n\n" + "="*20 + 'Creating Application Network Profiles and End Point Groups for each existing Vlan.' + "="*20
     print "="*10 + "Processing VLAN file " + vlan_csv +' for new Tenant ' + tenant_name + ' by user ' + username + " on APIC " + hostname + "="*10
 
+    # Call the extract_vlan function, pass it the filename provided in the command line
+    # This function will return a dictionary of vlan_id keys and vlan_name values
     vlans = extract_vlans(vlan_csv)
+    # Now that we have a dictionary of our anp=>epg pairs, generat the ANP and EPG names to be configured in the APIC
+    # Store those names in the epg_anp_dict dictionary for later use.
     for vlan_id,vlan_name in vlans.items():
         #print vlan_id, vlan_name
         epg_name = "v"+vlan_id.strip()+"_"+vlan_name.upper().strip()+"_EPG"
@@ -91,28 +107,48 @@ def main():
     #print epg_anp_dict
 
     print "="*10 + "Attempting to create Tenant " + tenant_name + ' by user ' + username + " on APIC " + hostname + "="*10
-
     print "\n\tLogging in to APIC"
+    # Establish a session to the apic whose parameters/credentials were passed as arguments on the command line
     apic_session = apic_login_acitk(hostname, username, password)
+
+    # Call the print_tenant function which will return a list of all the tenants currently configured on the APIC
+    # This list will be used to check to see if the tenant to be configured provided via an argument already exists
     tenants = print_tenant(apic_session)
     #print "modir ",modir
     #print "tenant_name ", tenant_name
+    # If the tenant we are supposed to create already exists, primt a status message, print all the existing tenants, and exit the script.
     if tenant_name in tenants:
 
         print "\n\t Tenant <"+tenant_name+ "> already exists and does not need to be created.\n"
         for t in tenants:
             print "\t Tenant "+t + " exists."
-        print "\t\n Exiting Script.\n"
-        sys.exit()
 
+        delete = raw_input("\n**** Would you like to delete the existing tenant " + tenant_name + " (Yes/No)? ****: ")
+
+        #print "delete", str(delete)
+
+        if delete.lower().strip() == 'no':
+
+            print "\t\n Exiting Script.\n"
+            sys.exit()
+
+        else:
+            delete2 = raw_input("\n**** Are you absolutely sure you want to delete the existing tenant <" + tenant_name + "> (Yes/No)? ****: ")
+            print "\nSorry, I still need to figure out how to delete a tenant."
+            print "\t\nExiting Script.\n"
+            sys.exit()
+
+    # If the tenant does not exist, create it and create the corresponding VRF
     else:
 
-        print "\n\tCreating Tenant " + tenant_name + "\n"
+        print "\n\tCreating Tenant " + tenant_name
         #create_tenant(modir, tenant_name)
+        # Create the tenant
         tenant = ACI.Tenant(tenant_name)
 
         #Configure a Private Network/VRF for the Tenant
         tenant_vrf = tenant_name + "_VRF"
+        print "\n\tCreating Private Network\VRF " + tenant_vrf + " for Tenant " + tenant_name + "\n"
         context = ACI.Context(tenant_vrf, tenant)
 
 
